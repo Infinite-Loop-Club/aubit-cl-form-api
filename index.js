3edf0b1bd8e5dd3816590cd552c6540fa26118b1
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-09-13 19:26:48 +0530

    fix: fixed errors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const helmet = require("helmet");
 const connection = require("./connection");
 const logger = require("./logger");
--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const winston = require("winston");
 
 const logger = winston.createLogger({
   transports: [
-    winston.transports.Console,
+    new winston.transports.Console(),
     new winston.transports.File({
       filename: "log.log",
       level: "warn",
